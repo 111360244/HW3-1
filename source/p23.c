6ab8b98b0e5c9da73e7dd49d6a09e144f1a6d98e
111360244 <minyanzhuang9@gmail.com>
2022-10-25 00:55:59 +0800

    HW3

---
+++ source/p23.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum Status {CONTINUE, WON, LOST};
+int rollDice(void);
+
+int main(void)
+{
+	int sum;
+	int myPoint;
+
+	enum Status gameStatus;
+
+	srand(time(NULL));
+	sum = rollDice();
+
+	switch (sum)
+	{
+	case 7:
+	case 11:
+		gameStatus = WON;
+		break;
+	case 2:
+	case 3:
+	case 12:
+		gameStatus = LOST;
+		break;
+	default:
+		gameStatus = CONTINUE;
+		myPoint = sum;
+		printf("Point is %d\n", myPoint);
+		break;
+	}
+	while (gameStatus == CONTINUE)
+	{
+		sum = rollDice();
+		if (sum == myPoint)
+			gameStatus = WON;
+		else
+			if (sum == 7)
+				gameStatus = LOST;
+	}
+	if (gameStatus == WON)
+		printf("Player wins\n");
+	else
+		printf("Player loses\n");
+
+}
+
+int rollDice(void)
+{
+	int diel,diel2,workSum;
+
+	diel = 1 + (rand() % 6);
+	diel2 = 1 + (rand() % 6);
+	workSum = diel + diel2;
+	printf("Player rolled %d + %d= %d\n", diel, diel2, workSum);
+	return workSum;
+
+}
